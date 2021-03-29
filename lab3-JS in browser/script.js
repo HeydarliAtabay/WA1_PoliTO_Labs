7f637e8e7418d3c1de7f573acbe0f6f70f6556d9
Atabay Heydarli <heydarli.atabay@gmail.com>
2021-03-29 17:39:46 +0200

    Minor changes on task icons

--- lab3-JS in browser/script.js
+++ lab3-JS in browser/script.js
@@ -29,13 +29,17 @@ function createTaskNode(task){
     externalDiv.appendChild(dateText);
     
     if(!task.private){
-        innerDiv.insertAdjacentHTML("afterend", ` <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#0d0d0d" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-        <rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect><path d="M7 11V7a5 5 0 0 1 10 0v4"></path></svg> `);
-    }
-    else{
+       
         innerDiv.insertAdjacentHTML("afterend", ` <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#0d0d0d" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
         <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path><polyline points="22 4 12 14.01 9 11.01"></polyline></svg>` )
+       
     }
+    else{
+      
+        innerDiv.insertAdjacentHTML("afterend", ` <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#0d0d0d" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+        <rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect><path d="M7 11V7a5 5 0 0 1 10 0v4"></path></svg> `);
+   
+          }
     
     li.appendChild(externalDiv);
     return li;
